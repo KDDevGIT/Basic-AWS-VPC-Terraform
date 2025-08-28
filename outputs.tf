79de16f87e4371a09eafc3075b8e410c08040b29
Kyler Dana <kymasterbox@gmail.com>
2025-08-28 09:58:37 -0700

    Added public and private route table IDs to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,14 @@ output "private_subnet_ids" {
     value = module.vpc.private_subnet_ids
 }
 
+# Public Route Table ID
+output "public_route_table_id" {
+    value = module.vpc.public_route_table_id
+}
+
+# Private Route Table ID
+output "private_route_table_id" {
+    value = module.vpc.private_route_table_id
+}
+
+
