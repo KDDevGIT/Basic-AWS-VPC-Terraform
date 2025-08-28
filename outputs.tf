c4d11a7c6bc89d0bbd6b5a38384cab8c5ff37ea3
Kyler Dana <kymasterbox@gmail.com>
2025-08-28 09:56:39 -0700

    Added public and private subnet ID outputs to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,14 @@
 output "vpc_id" {
     value = module.vpc.vpc_id
 }
+
+# Public Subnet IDs
+output "public_subnet_ids" {
+    value = module.vpc.public_subnet_ids
+}
+
+# Private Subnet IDs
+output "private_subnet_ids" {
+    value = module.vpc.private_subnet_ids
+}
+
