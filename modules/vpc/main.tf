37bd5934c0162dce81d684e2516b1ec9ea5ffe9e
Kyler Dana <kymasterbox@gmail.com>
2025-08-28 11:29:35 -0700

    Added AWS VPC Resource to module/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,3 +12,12 @@ locals {
     }
 }
 
+# AWS VPC Resource 
+resource "aws_vpc" "this" {
+    cidr_block = var.cidr_block
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags = merge(var.tags, { Name = "${var.name}-vpc"})
+}
+
+
